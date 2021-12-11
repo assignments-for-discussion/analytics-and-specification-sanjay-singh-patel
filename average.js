d32c62e35e354a13770d18197ac395988ecf440e
Sanjay Singh Patel <sanjupatel11112000@gmail.com>
2021-12-11 16:33:37 +0530

    Update avrage.js

--- average.js
+++ average.js
@@ -1,12 +1,13 @@
-function av(p,c){
-  if (isNaN(c)){
-    n-=1
-    return p;
-  }
-  return p+c;
-}
 function average(numbers) {
-  n=numbers.length
-  return numbers.reduce(av, 0) / n;
+  n = numbers.length
+  return numbers.reduce((p,c)=>{
+    if (isNaN(c))
+    {
+      n-=1
+      return p;
+    }
+    return p+c;
+  }, 0) / n;
 }
+
 module.exports = {average};
