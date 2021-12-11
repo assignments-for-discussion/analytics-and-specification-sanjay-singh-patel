8c50f306cff841aba595c8f01651f32a6f48c8d4
Sanjay Singh Patel <sanjupatel11112000@gmail.com>
2021-12-11 15:55:20 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,10 +1,7 @@
 
 function average(numbers) {
-<<<<<<< HEAD
-  return numbers.reduce((p,c)=>{if (isNaN(c))
-=======
-  return numbers.reduce((p,c)=>{if (isNaN(c))
->>>>>>> 74adac7aadd380e86fed4b378505dad827ba2398
+  return numbers.reduce((p,c)=>{
+    if (isNaN(c))
     {
       numbers.length-=1
       return p;
