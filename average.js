852ea8c16bde835a23ee7f01d071a6aaae1322c4
Sanjay Singh Patel <sanjupatel11112000@gmail.com>
2021-12-15 18:01:07 +0530

    Updated average.js and test/average.test.js

--- average.js
+++ average.js
@@ -3,19 +3,27 @@ function average(numbers) {
   // counting the number of NaNs and outlier values
   for(var i=0;i<numbers.length;i++)
   {
-    if(isNaN(numbers[i]) ||numbers[i]>=150 || numbers[i]<0)
+    if(isNaN(numbers[i]))
     {
+      n-=1
+    }
+    if (numbers[i]>=150 || numbers[i]<0){
       n-=1
     }
   }
   // returning the average
   return numbers.reduce((p,c)=>
   {
-    // ignoring a value if it is NaN and assuming any value grater tha 150 or negative is an outlier value
-    if (isNaN(c)||c>=150||c<0){
+    // ignoring a value if it is NaN
+    if (isNaN(c)){
+      return p;
+    }
+    // Assuming any value grater tha 150 or negative is an outlier value
+    if(c>=150||c<0){
       return p;
     }
     return p+c;
   }, 0) / n;
 }
+console.log(average([51, 1000, 38, 71, 80, 1500, -15]))
 module.exports = {average};
