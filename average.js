75425744cd3431d4667e11e84fde11ebc90bd90c
Sanjay Singh Patel <sanjupatel11112000@gmail.com>
2021-12-15 17:50:04 +0530

    Test and Implementation for outlier values
    I made the asked changes and wrote a unit test and also wrote a condition to rule out the any outlier values. I have considered any negative value or any value greater than 150 to be outlier.

--- average.js
+++ average.js
@@ -1,9 +1,18 @@
 function average(numbers) {
-  var n =numbers.length
-  return numbers.reduce((p,c)=>{
-    if (isNaN(c))
+  var n = numbers.length
+  // counting the number of NaNs and outlier values
+  for(var i=0;i<numbers.length;i++)
+  {
+    if(isNaN(numbers[i]) ||numbers[i]>=150 || numbers[i]<0)
     {
       n-=1
+    }
+  }
+  // returning the average
+  return numbers.reduce((p,c)=>
+  {
+    // ignoring a value if it is NaN and assuming any value grater tha 150 or negative is an outlier value
+    if (isNaN(c)||c>=150||c<0){
       return p;
     }
     return p+c;
--- test/average.test.js
+++ test/average.test.js
@@ -14,3 +14,7 @@ it('reports the average as NaN on an empty list', ()=> {
 it('ignores NaN in the input', ()=> {
   expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
 });
+// Testcase to check if outlier valuer are ignored
+it('ignores outlier values in the input', ()=> {
+  expect(average([51, 1000, 38, 71, 80, 1500, -15])).to.be.approximately(60, 0.01);
+});
