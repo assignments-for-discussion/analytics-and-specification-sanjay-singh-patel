f463114d450f6336d54add34ae2e2439d6245356
Sanjay Singh Patel <sanjupatel11112000@gmail.com>
2021-12-15 20:07:35 +0530

    update average.js

--- average.js
+++ average.js
@@ -1,7 +1,7 @@
 function average(numbers) {
-  var n = t = numbers.length
+  var n = numbers.length
   // counting the number of NaNs and outlier values
-  for(var i=0;i<t;i++)
+  for(var i=0;i<numbers.length;i++)
   {
     if(isNaN(numbers[i])||numbers[i]>=150 || numbers[i]<0){
       n-=1
@@ -22,5 +22,4 @@ function average(numbers) {
   }, 0) / n;
   return avg
 }
-console.log(average([51, 1000, 38, 71, 80, 1500, -15]))
 module.exports = {average};
